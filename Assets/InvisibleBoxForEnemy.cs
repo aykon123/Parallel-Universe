dd1c03385e11f5c4bd1fee2909019574020c49a4
Yekta Kücük <yk020@hdm-stuttgart.de>
2023-06-25 23:07:35 +0200

    kleine tests

--- Assets/InvisibleBoxForEnemy.cs
+++ Assets/InvisibleBoxForEnemy.cs
@@ -8,18 +8,18 @@ public class CollisionBlocker : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            // Setze den Enemy als Kindobjekt dieses GameObjects
-            collision.gameObject.transform.SetParent(transform);
+            // Stop the enemy's movement
+            Rigidbody2D enemyRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+            enemyRigidbody.velocity = Vector2.zero;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            // Entferne den Enemy als Kindobjekt
-            collision.gameObject.transform.SetParent(null);
+            // Update the enemy's position to match the blocker's position
+            collision.gameObject.transform.position = transform.position;
         }
     }
 }
-
--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -16,11 +16,24 @@ public class EnemyPatrol : MonoBehaviour
     public Transform playerTransform;
     public bool isChasing;
     public float chaseDistance;
+    public Transform stopPosition; // Stop position for chasing
+    public float stopDistanceThreshold = 0.1f; // Distance threshold to stop chasing
+
+    private bool hasReachedStopPosition; // Flag to track if the enemy has reached the stop position
+    private bool returningToPatrol; // Flag to track if the enemy is returning to the patrol route
 
     private void Update()
     {
         if (isChasing)
         {
+            if (Vector2.Distance(transform.position, stopPosition.position) <= stopDistanceThreshold)
+            {
+                isChasing = false; // Stop chasing if within stop distance threshold
+                hasReachedStopPosition = true;
+                returningToPatrol = true;
+                return;
+            }
+
             float direction = Mathf.Sign(playerTransform.position.x - transform.position.x);
             transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
 
@@ -30,11 +43,39 @@ public class EnemyPatrol : MonoBehaviour
                 FlipSprite();
             }
         }
+        else if (returningToPatrol)
+        {
+            Transform destination = patrolPoints[0]; // Return to patrol point 0
+
+            // Move towards the patrol point
+            transform.position = Vector2.MoveTowards(transform.position, destination.position, moveSpeed * Time.deltaTime);
+
+            // Check if reached the patrol point
+            if (Vector2.Distance(transform.position, destination.position) < 0.2f)
+            {
+                returningToPatrol = false; // Stop returning to patrol
+                hasReachedStopPosition = false; // Reset the flag for the next patrol destination
+            }
+            else
+            {
+                // Flip the sprite based on the movement direction
+                float direction = Mathf.Sign(destination.position.x - transform.position.x);
+                if ((direction > 0 && !facingRight) || (direction < 0 && facingRight))
+                {
+                    FlipSprite();
+                }
+            }
+        }
         else
         {
             if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
             {
                 isChasing = true;
+                hasReachedStopPosition = false;
+            }
+            else if (hasReachedStopPosition) // Return to patrol if not chasing and has reached the stop position
+            {
+                returningToPatrol = true;
             }
             else
             {
@@ -47,16 +88,20 @@ public class EnemyPatrol : MonoBehaviour
 
                 // Move towards the current patrol destination
                 Transform currentDestination = patrolPoints[patrolDestination];
-                transform.position = Vector2.MoveTowards(transform.position, currentDestination.position, moveSpeed * Time.deltaTime);
+                float direction = Mathf.Sign(currentDestination.position.x - transform.position.x);
+                transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+
+                // Flip the sprite based on the direction
+                if ((direction > 0 && !facingRight) || (direction < 0 && facingRight))
+                {
+                    FlipSprite();
+                }
 
                 // Check if reached the patrol destination
                 if (Vector2.Distance(transform.position, currentDestination.position) < 0.2f)
                 {
                     // Switch to the next patrol destination
                     patrolDestination = (patrolDestination + 1) % patrolPoints.Length;
-
-                    // Flip the sprite horizontally
-                    FlipSprite();
                 }
             }
         }
@@ -71,3 +116,4 @@ public class EnemyPatrol : MonoBehaviour
         transform.localScale = newScale;
     }
 }
+
