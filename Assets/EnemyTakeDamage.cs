e1438b4c3cdfc389b99cea951408a57fb1bc0807
Yekta Kücük <yk020@hdm-stuttgart.de>
2023-06-28 22:46:00 +0200

    Attack testing

--- Assets/EnemyTakeDamage.cs
+++ Assets/EnemyTakeDamage.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class EnemyTakeDamage : MonoBehaviour
 {
     private Animator anim;
     [SerializeField] private float bounceForce = 10f;
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
 
     private Rigidbody2D enemyRb;
 
@@ -15,6 +18,7 @@ public class EnemyTakeDamage : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         enemyRb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,16 +31,22 @@ public class EnemyTakeDamage : MonoBehaviour
             // Check if the player is moving downward (i.e., jumping on the enemy)
             if (playerRb.velocity.y <= 0)
             {
-                // Destroy the enemy object
-                if (anim != null)
+                // Destroy the enemy object if health reaches 0
+                if (currentHealth <= 0)
                 {
                     enemyHeadJumpSoundEffect.Play();
                     anim.SetTrigger("enemy_death");
 
                     // Disable the enemy's Rigidbody2D component to detach it from the player
                     enemyRb.simulated = false;
+                    Invoke("DestroyEnemy", 0.5f);
                 }
-                Invoke("DestroyEnemy", 0.5f);
+                else
+                {
+                    // Apply damage and bounce force to the enemy
+                    currentHealth -= 1;
+                    enemyRb.velocity = new Vector2(enemyRb.velocity.x, bounceForce);
+                }
 
                 // Apply a bounce force to the player in the opposite vertical direction
                 playerRb.velocity = new Vector2(playerRb.velocity.x, bounceForce);
@@ -44,9 +54,14 @@ public class EnemyTakeDamage : MonoBehaviour
         }
     }
 
+    public void TakeDamage (int damage)
+    {
+        currentHealth -= damage;
+        UnityEngine.Debug.Log("damage taken!");
+    }
+
     private void DestroyEnemy()
     {
         Destroy(gameObject);
     }
 }
-
---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.K)) // Check for the "K" key press
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyTakeDamage>().TakeDamage(1); // Pass the damage value here
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+
+    void OnDrawGizmoSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
